9781cfb02a5d69979c9b37b395717996df876b1d
Mikhail Kotlik <mkotlik@stuy.edu>
2016-09-20 16:22:10 -0400

    Finished flask-template app

--- FirstApp.py
+++ FirstApp.py
@@ -4,14 +4,31 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-	return "Welcome to Misha's homepage!"
+	return '''<h1>Welcome to Misha's homepage!</h1><br>
+		Go to:<br>
+		<a href="/about">About</a><br>
+		<a href="/revolution">Revolution</a>
+		'''
 
 @app.route("/about")
 def about():
-	return "This is a test Flask app made by Misha"
+	return '''<h1>About</h1><br>
+		This is a test Flask app made by Misha<br>
+		<br>
+		Go to:<br>
+		<a href="/">Home</a><br>
+		<a href="/revolution">Revolution</a>
+		'''
 
 @app.route("/revolution")
 def revolution():
-	return 'Click <a href="https://www.youtuve.com/watch?v=w8KQmps-Sog">here</a> for a cool video from MUSE'
+	return '''<h1>Revolution</h1><br>
+		Click <a href="https://www.youtube.com/watch?v=w8KQmps-Sog">here</a> for a cool video from MUSE<br>
+		<br>
+		Go to:<br>
+		<a href="/">Home</a><br>
+		<a href="/about">About</a>
+		'''
+
 
 app.run()
